eb533c250dd7ab84948eb748d1f505b8623befb1
parrenin <parrenin.ujf@gmail.com>
2014-04-16 15:23:59 +0200

    dev/: sigmap_corr_tau now linearly related to iedepth. It now converges in a reasonable time (~10 mn).

--- dev/parameters-AllDrillings.py
+++ dev/parameters-AllDrillings.py
@@ -7,7 +7,7 @@ self.lambda_tau=70
 self.lambda_a=4000
 self.lambda_LID=4000
 self.Dfirn=0.7            #Average density of the firn
-self.k=0.1
+self.k=0.5
 self.sigmap_corr_a=0.2
 self.sigmap_corr_LID=0.4
 
--- dev/parameters-CovariancePrior-AllDrillings-init.py
+++ dev/parameters-CovariancePrior-AllDrillings-init.py
@@ -15,6 +15,6 @@ g=interpolate.interp1d(np.array([0,self.lambda_tau,5000]),np.array([1, 0, 0]))
 self.correlation_corr_tau=g(np.abs(np.ones((np.size(self.corr_tau_depth),np.size(self.corr_tau_depth)))*self.corr_tau_depth-np.transpose(np.ones((np.size(self.corr_tau_depth),np.size(self.corr_tau_depth)))*self.corr_tau_depth)))
 
 #        f=interpolate.interp1d(self.depth,self.udepth_init)
-f=interpolate.interp1d(self.depth,self.udepth_model, bounds_error=False, fill_value=self.udepth_model[-1]) #We should not need the bounds_error option. Check what is the problem.
+f=interpolate.interp1d(self.depth,self.iedepth, bounds_error=False, fill_value=self.iedepth[-1]) #We should not need the bounds_error option. Check what is the problem.
 self.sigmap_corr_tau=self.k/self.thickness_ie*f(self.corr_tau_depth)
 
