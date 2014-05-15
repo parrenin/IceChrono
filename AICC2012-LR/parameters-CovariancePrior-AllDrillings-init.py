21da654ef42afefcfadd198e909243b72f7ebe5e
parrenin <parrenin.ujf@gmail.com>
2014-05-15 18:23:16 +0200

    AICC2012-LR: More work on the online calculations of the sigmas. Not perfect yet.

--- AICC2012-LR/parameters-CovariancePrior-AllDrillings-init.py
+++ AICC2012-LR/parameters-CovariancePrior-AllDrillings-init.py
@@ -19,7 +19,7 @@ self.correlation_corr_tau=g(np.abs(np.ones((np.size(self.corr_tau_depth),np.size
 #correlation_corr_tau=gaussian(np.abs(np.ones((np.size(self.corr_tau_depth),np.size(self.corr_tau_depth)))*self.corr_tau_depth-np.transpose(np.ones((np.size(self.corr_tau_depth),np.size(self.corr_tau_depth)))*self.corr_tau_depth))/self.lambda_tau)
 
 
-#Now we define the sigmas
+##Now we define the sigmas
 
 #sigmap_corr_a
 readarray=np.loadtxt(datadir+self.label+'/accu-sigma-prior.txt')
@@ -55,7 +55,7 @@ self.sigmap_corr_LID=f(self.corr_LID_age)
 #    mAloc[i]=np.ma.average(self.a_model, weights=weights)
 
 ##        f=interpolate.interp1d(self.depth,self.udepth_init)
-#f=interpolate.interp1d(self.depth,self.udepth_init, bounds_error=False, fill_value=self.udepth_init[-1]) #We should not need the bounds_error option. Check what is the problem.
+#f=interpolate.interp1d(self.depth,self.udepth_model, bounds_error=False, fill_value=self.udepth_model[-1]) #We should not need the bounds_error option. Check what is the problem.
 #g=interpolate.interp1d(self.depth[:-1], sigmaAloc, bounds_error=False, fill_value=sigmaAloc[-1])
 #self.sigmap_corr_tau=self.cT1+self.cT2*f(self.corr_tau_depth) + self.cT3*g(self.corr_tau_depth)/max(sigmaAloc)
 
@@ -64,8 +64,8 @@ self.sigmap_corr_LID=f(self.corr_LID_age)
 #weights=np.where(self.age_model[:-1]<12000, self.age_model[1:]-self.age_model[:-1], 0)
 #A0=np.ma.average(self.a_model, weights=weights)
 #self.sigmap_corr_a=self.sigmabA*np.abs(A0-f(self.corr_a_age))/max(np.abs(A0-f(self.corr_a_age)))*(1+self.cA1*g(self.corr_a_age)/self.thickness)
-#self.sigmap_corr_a=np.where(self.sigmap_corr_a<self.sigmam, self.sigmam*(1+self.cA1*g(self.corr_a_age)/self.thickness), self.sigmap_corr_a)
+#self.sigmap_corr_a=np.where(self.sigmap_corr_a<self.sigmam*(1+self.cA1*g(self.corr_a_age)/self.thickness), self.sigmam*(1+self.cA1*g(self.corr_a_age)/self.thickness), self.sigmap_corr_a)
 
 #f=interpolate.interp1d(self.age_model[:-1], mAloc, bounds_error=False, fill_value=mAloc[-1])
 #g=interpolate.interp1d(self.corr_a_age, self.sigmap_corr_a, bounds_error=False, fill_value=self.sigmap_corr_a[-1])
-#self.sigmap_corr_LID=self.sigmabL/self.sigmabA*g(self.corr_LID_age)/(1+f(self.corr_LID_age)/max(mAloc))
+#self.sigmap_corr_LID=self.sigmabL/self.sigmabA*g(self.corr_LID_age)/(1+f(self.corr_LID_age))
