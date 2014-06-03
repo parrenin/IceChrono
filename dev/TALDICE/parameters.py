c951ba1d0912c92fc8b9cc0ed29b342ccf28515e
PARRENIN Frederic <fparrenin@LGGE091.lgge.lan>
2014-06-03 17:54:12 +0200

    Bringing back the dev experiment.

---
+++ dev/TALDICE/parameters.py
@@ -0,0 +1,26 @@
+#Parameters specific to the TALDICE ice core, from the Gnumeric TALDICE file
+#Changelog: modified the udepth.txt file to prevent an error during the leastsq function
+#TODO: use the real EDC-TALDICE ice-ice strati links with small errors by using a restart file
+#TODO: TALDICE does not run alone. Check what is the problem.
+
+self.dim=1
+self.calc_a=True
+self.calc_a_method='fullcorr'
+self.calc_tau=True
+self.calc_LID=True
+self.udepth_top=0.
+self.age_top=-50
+self.depth=np.arange(0., 1620.+0.01, 1.)
+self.gamma_source=3.4 #Same as DC, need to check with B. Stenni if there is a better value
+self.beta_source=1.5  #Same as DC, need to check with B. Stenni if there is a better value
+self.A0=8.26e-02
+self.beta=6.64e-03
+self.thickness=1620. #1650 is the documented value but we need 1620 for the code to not crash
+self.pprime=m.log(1.47+1)
+self.s=0.
+self.muprime=m.log(1.57e-02)
+self.corr_a_age=np.arange(-50, 500000-50+0.01, self.age_step)
+self.corr_LID_age=np.arange(-50, 500000-50+0.01, self.age_step)
+self.corr_tau_depth=np.arange(self.depth[0], self.depth[-1]+0.01, (self.depth[-1]-self.depth[0])/(self.corr_tau_nodes-1))
+self.LID_value=76.
+self.restart=False
