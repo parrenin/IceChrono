0f3f4893abdb0ecb5d39ee643bc896d7ac72b178
PARRENIN Frederic <fparrenin@LGGE091.lgge.lan>
2014-09-19 12:11:37 +0200

    Update Clean.py for .bak files.

--- Clean.py
+++ Clean.py
@@ -50,3 +50,15 @@ for root, dirs, files in os.walk(dir, topdown=False):
         path = os.path.join(root, name)
         if len(os.listdir(path)) == 0:
             os.rmdir(path)
+
+pattern='.bak'
+regexObj = re.compile(pattern)
+for root, dirs, files in os.walk(dir, topdown=False):
+    for name in files:
+        path = os.path.join(root, name)
+        if bool(regexObj.search(path)) == bool(inclusive):
+            os.remove(path)
+    for name in dirs:
+        path = os.path.join(root, name)
+        if len(os.listdir(path)) == 0:
+            os.rmdir(path)
